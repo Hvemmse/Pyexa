8f9424f17c1901aaecd46ba747dce2cf407f6547
Frank Simens <84502105+Hvemmse@users.noreply.github.com>
2022-12-27 10:54:35 +0100

    a little password generator example
    written in python

---
+++ pwkgos.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+import random
+import string
+import pyperclip
+
+# Create the main window
+window = tk.Tk()
+window.title("Password Generator")
+
+# Create a function to generate the password
+def generate_password():
+  # Set the length of the password
+  password_length = 16
+
+  # Create an empty list to store the characters of the password
+  password_chars = []
+
+  # Generate the password by adding random characters to the list
+  for i in range(password_length):
+    # Choose a random character type (lowercase, uppercase, or digits)
+    char_type = random.choice(['lower', 'upper', 'digits'])
+
+    # Add a random character of the chosen type to the list
+    if char_type == 'lower':
+      password_chars.append(random.choice(string.ascii_lowercase))
+    elif char_type == 'upper':
+      password_chars.append(random.choice(string.ascii_uppercase))
+    elif char_type == 'digits':
+      password_chars.append(random.choice(string.digits))
+
+  # Convert the list of characters to a string and return it
+  return ''.join(password_chars)
+
+# Create a function to copy the password to the clipboard
+def copy_password():
+  # Copy the password to the clipboard
+  pyperclip.copy(password_label['text'])
+
+# Create a label to display the password
+password_label = tk.Label(window, text="")
+password_label.pack()
+
+# Create a button to generate a new password
+generate_button = tk.Button(window, text="Generate Password", command=lambda: password_label.config(text=generate_password()))
+generate_button.pack()
+
+# Create a button to copy the password to the clipboard
+copy_button = tk.Button(window, text="Copy Password", command=copy_password)
+copy_button.pack()
+
+# Run the Tkinter event loop
+window.mainloop()
